632f6bc1de9e6e0d8685a36df462dfdefe35eb02
Hasitha Liyanage <hasitha@thinkcube.com>
2015-07-27 20:31:08 +0530

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,20 @@ var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
   entry: [path.resolve(ROOT_PATH, 'app/main')],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel?stage=1',
+        include: path.resolve(ROOT_PATH, 'app')
+      },
       {
         test: /\.css$/,
         loaders: ['style', 'css']

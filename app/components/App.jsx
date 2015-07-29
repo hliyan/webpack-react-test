d4972c2a8db537bfb4bc3c54632eeca97491dc33
Hasitha Liyanage <hasithaliyan@gmail.com>
2015-07-29 17:29:44 +0530

    rendering notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,7 +2,37 @@ import React from 'react';
 import Note from './Note';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.renderNote = this.renderNote.bind(this);
+  }
+
   render() {
-    return <Note />;
+    const notes = [
+      {
+        task: 'Learn webpack'
+      },
+      {
+        task: 'Learn React'
+      },
+      {
+        task: 'Do laundry'
+      }
+    ];
+
+    return (
+      <div>
+        <ul>{notes.map(this.renderNote)}</ul>
+      </div>
+    );
+  }
+  
+  renderNote(note, i) {
+    return (
+      <li key={`note${i}`}>
+        <Note value={note.task} />
+      </li>
+    );
   }
 }

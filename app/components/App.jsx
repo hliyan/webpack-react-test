46d10b45988a153b9682df287d09267e0d6947d9
Hasitha Liyanage <hasithaliyan@gmail.com>
2015-08-01 19:04:19 +0530

    re-added hard coded new note function

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,26 +4,44 @@ import findIndex from '../libs/find_index';
 import uuid from 'node-uuid';
 
 export default class App extends React.Component {
-  render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+  constructor(props) {
+    super(props);
+    this.addItem = this.addItem.bind(this);
 
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const notes = this.state.notes;
     return (
       <div>
+        <button onClick={this.addItem}>+</button>
         <Notes items={notes} />
       </div>
     );
+  }
+
+  addItem() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
   }
 }

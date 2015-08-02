42a5596d91932ec08c006a98da423b234bc6497e
Hasitha Liyanage <hasithaliyan@gmail.com>
2015-08-02 18:40:09 +0530

    integrated AltContainer

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,43 +1,30 @@
 import uuid from 'node-uuid';
+import AltContainer from 'alt/AltContainer';
 import React from 'react';
 import Notes from './Notes';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.storeChanged = this.storeChanged.bind(this);
-    this.state = NoteStore.getState();
-  }
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-  storeChanged(state) {
-    this.setState(state);
-  }
+export default class App extends React.Component {  
   render() {
-    const notes = this.state.notes;
-
     return (
       <div>
         <button onClick={this.addItem}>+</button>
-        <Notes items={notes} onEdit={this.itemEdited} />
+        <AltContainer stores={[NoteStore]} inject={ { items: () => NoteStore.getState().notes } } >
+          <Notes onEdit={this.itemEdited} />
+        </AltContainer>
       </div>
     );
   }
+
   addItem() {
     NoteActions.create({id: uuid.v4(), task: 'New task'});
   }
+  
   itemEdited(id, task) {
     if(task) {
       NoteActions.update({id, task});
-    }
-    else {
+    } else {
       NoteActions.delete(id);
     }
   }

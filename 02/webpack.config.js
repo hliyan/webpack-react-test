2d82fbf63bcb2ec5c66a50ae879cfbca5166d43d
Hasitha Liyanage <hasithaliyan@gmail.com>
2015-08-05 18:04:17 +0530

    minor

--- 02/webpack.config.js
+++ 02/webpack.config.js
@@ -4,12 +4,14 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var ROOT_PATH = path.resolve(__dirname);
 
 module.exports = {
-  entry: path.resolve(ROOT_PATH, 'index'),
+  entry: path.resolve(ROOT_PATH, 'index.js'),
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
   plugins: [
-    new HtmlwebpackPlugin({ title: 'React Flux Test' })
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
   ]
 };

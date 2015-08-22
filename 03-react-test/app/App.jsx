3705d8a0e5ec50f44cc13205c2678407f4cf0138
Hasitha Liyanage <hasithaliyan@gmail.com>
2015-08-22 10:27:27 +0530

    simple event handler

--- 03-react-test/app/App.jsx
+++ 03-react-test/app/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Field from './swisher-ui';
 
 export default class App extends React.Component {
     render() {
-        return (<div>Hello from react</div>);
+        return (
+            <div>
+                <Field label="name" value="joe" onKeyPress={this.onKeyPress} />
+            </div>
+        );
+    }
+
+    onKeyPress(e) {
+        if (e.key == 'Enter') {
+            alert(e.target.value);
+        }
     }
 }
